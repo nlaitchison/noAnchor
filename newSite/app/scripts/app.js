abb506d592e07ee1822b0f140d1697f620c03d48
Nicole Aitchison <nlaitchison@gmail.com>
2014-12-12 11:38:25 -0600

    checking user works

--- newSite/app/scripts/app.js
+++ newSite/app/scripts/app.js
@@ -22,6 +22,25 @@ $routeProvider
   });
 });
 
+App.run(['$rootScope', function($rootScope){
+
+	console.log('running');
+
+	// Create a callback which logs the current auth state
+	function authDataCallback(authData) {
+	  if (authData) {
+	    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+	  } else {
+	    console.log("User is logged out");
+	  }
+	}
+	
+	// Register the callback to be fired every time auth state changes
+	var ref = new Firebase("https://no-anchor.firebaseio.com");
+	ref.onAuth(authDataCallback);
+	
+	}]);
+
 /*
 App.directive('fadeLogo', function($window) {
   return {
--- newSite/app/scripts/controllers/AuthCtrl.js
+++ newSite/app/scripts/controllers/AuthCtrl.js
@@ -16,12 +16,27 @@ App.controller('AuthCtrl',['$rootScope', function($rootScope){
 	ref.authWithOAuthPopup("twitter", function(error, authData) {
 		if (error) {
 			console.log("Login Failed!", error);
-		} else {
+		} else { 
 			console.log("Authenticated successfully with payload:", authData);
+			checkUser(authData);
 		}
 	}, {
 	  remember: "sessionOnly"
 	});
+  };
+  
+  // check to make sure the user is an admin
+  var checkUser = function(authData){
+	console.log('check user', authData.uid); 
+	// if the user isn't the admin  
+	if(authData.uid != 'twitter:2361496574'){
+		// logout the user
+		ref.unauth();
+	}else{
+		// keep user logged in
+		console.log('user authorized');
+		//redirect user
+	}
   };
 
 }]);
